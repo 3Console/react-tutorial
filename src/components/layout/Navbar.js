f1a378eb3d8ca46c36e231d2f89315f8fa04ae10
3Console <quanghuy101202@gmail.com>
2020-08-22 15:18:16 +0700

    add link navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignInLinks from './SignInLinks';
+import SignOutLinks from './SignOutLinks';
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">ReactTutorial</Link>
+        <SignInLinks />
+        <SignOutLinks />
       </div>
     </nav>
   )
