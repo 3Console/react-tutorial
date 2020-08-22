869579409ab57d5101ae81af0ccd71657321977a
3Console <quanghuy101202@gmail.com>
2020-08-22 16:28:20 +0700

    create dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import DashBoard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/project/ProjectDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path='/' component={ DashBoard } />
+          <Route path='/project/:id' component={ ProjectDetails } />
+        </Switch>
       </div>
     </BrowserRouter>
   );
