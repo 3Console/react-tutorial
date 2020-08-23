67df2ddedd09083fca0fcd95b62d24e53baab84f
3Console <quanghuy101202@gmail.com>
2020-08-23 16:23:13 +0700

    add thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,6 +1,16 @@
-const initState = {};
+const initState = {
+  projects: [
+    {id: '1', title: 'Project A', content: 'blah blah blah'},
+    {id: '2', title: 'Project B', content: 'blah blah blah'},
+    {id: '3', title: 'Project C', content: 'blah blah blah'},
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT': 
+      console.log('created project', action.project);
+  }
   return state;
 }
 
