cf4064cd794f8f61327384e4a4771978d0435fd3
3Console <quanghuy101202@gmail.com>
2020-08-29 15:58:09 +0700

    sign up and set up profile

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,8 +5,8 @@ import SignOutLinks from './SignOutLinks';
 import { connect } from 'react-redux';
 
 const Navbar = (props) => {
-  const { auth } = props;
-  const links = auth.uid ? <SignInLinks /> : <SignOutLinks />
+  const { auth, profile } = props;
+  const links = auth.uid ? <SignInLinks profile={profile} /> : <SignOutLinks />
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
@@ -19,7 +19,8 @@ const Navbar = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   }
 }
 
--- src/components/layout/SignInLinks.js
+++ src/components/layout/SignInLinks.js
@@ -8,7 +8,11 @@ const SignInLinks = (props) => {
     <ul className="right">
       <li><NavLink to='/create'>New Project</NavLink></li>
       <li><a onClick={props.signOut}>Log out</a></li>
-      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>H</NavLink></li>
+      <li>
+        <NavLink to='/' className='btn btn-floating pink lighten-1'>
+          {props.profile.initials}
+        </NavLink>
+      </li>
     </ul>
   )
 }
